33ef8ef856903437935d00bce8249853a409792c
janwolzenburg <72450694+janwolzenburg@users.noreply.github.com>
2022-12-14 13:13:45 +0100

    Added rays class

---
+++ rays.cpp
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * @file   rays.cpp
+ * @brief  Implementations
+ *
+ * @author Jan Wolzenburg
+ * @date   December 2022
+ *********************************************************************/
+
+
+/*********************************************************************
+	Includes
+ *********************************************************************/
+#include <string>
+using std::string;
+
+#include "rays.h"
+
+
+/*********************************************************************
+   Implementations
+*********************************************************************/
+
+
+/*
+	ray implementation
+*/
+
+ray::ray( const vec3 v_, const pnt3 p_, const rayProperties properties_ )
+	: line( v_, p_ ),
+	properties( properties_ ){};
+
+ray::ray( const line line_, const rayProperties  properties_ )
+	: line( line_ ),
+	properties( properties_ ){};
+
+ray ray::convertTo( const cartCSys* const target ) const{
+	return ray{ this->line::convertTo( target ), properties };
+};
+
+double ray::getPara( const pnt3 p, bool* const success ) const{
+	double t = line::getPara( p, success );
+	*success = *success && ( t >= 0 );
+	return t;
+}
+
+ray ray::projectOnXYPlane( const cartCSys* const cSys ) const{
+	return ray{ this->line::projectOnXYPlane( cSys ), this->properties };
+}
--- rays.h
+++ rays.h
@@ -6,4 +6,89 @@
  * @author Jan Wolzenburg
  * @date   December 2022
  *********************************************************************/
+ 
 
+
+ /*********************************************************************
+	Includes
+ *********************************************************************/
+ #include "line.h"
+ #include "operations.h"
+
+
+
+  /*********************************************************************
+	Definitions
+ *********************************************************************/
+
+/*!
+ * @brief Class for ray properties
+*/
+class rayProperties{
+	public:
+	double intensity = 0;		/*!< Intensity of ray */
+};
+
+
+/*!
+ * @brief Class for rays
+*/
+class ray : public line{
+	public:
+
+	/*!
+	 * @brief Constructor
+	 * @param v_ Trajectory
+	 * @param p_ O
+	 * @param intensity_ Intensity
+	*/
+	explicit ray( const vec3 v_, const pnt3 p_, const rayProperties properties_ );
+
+	/*!
+	 * @brief Constructor
+	 * @param line_ Line
+	 * @param intensity_ Intensity
+	*/
+	explicit ray( const line line_, const rayProperties  properties_ );
+
+	/*!
+	 * @brief Get intensity
+	 * @return Intensity
+	*/
+	inline rayProperties Properties( void ) const{ return properties; };
+
+	/*!
+	 * @brief Convert ray components to different coordinate system
+	 * @param target Target system
+	 * @return Ray in target system
+	*/
+	ray convertTo( const cartCSys* const target ) const;
+
+	/*!
+	 * @brief Get ray parameter corresponding to point
+	 * @param p Point on ray
+	 * @param success Is set to true when the given point lies on the ray. False if not
+	 * @return Ray parameter
+	*/
+	double getPara( const pnt3 p, bool* const success ) const;
+
+	/*!
+	 * @brief Calculate intersection between this ray and surface
+	 * @param sur Surface to intersect with
+	 * @return Instersection result
+	*/
+	//linSurf_Intersection_Result isectSurf( const surfLim sur ) const;
+
+	/*!
+	 * @brief Project ray on XY plane of coordinate system
+	 * @param cSys System to project on
+	 * @return Projected ray
+	*/
+	ray projectOnXYPlane( const cartCSys* const cSys ) const;
+
+	inline bool paraInBounds( const double para ) const{ return para >= 0; };
+
+
+	private:
+	rayProperties properties;			/*!< Property of ray*/
+};
