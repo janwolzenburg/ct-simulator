38fed0a134dba6820f9c63bb59b69e9f024d07be
Jan Wolzenburg <kontakt@janwolzenburg.de>
2023-01-10 10:28:07 +0100

    Deactivated tests

--- main.cpp
+++ main.cpp
@@ -19,15 +19,15 @@ using std::cerr;  using std::endl;
 */
 int main( void ){
 
-	if( !test_linear_algebra() ) cerr << "Test for linear algebra failed!";
+	//if( !test_linear_algebra() ) cerr << "Test for linear algebra failed!";
 
-	if( !test_tube() ) cerr << "Test for xRay tube failed!";
+	//if( !test_tube() ) cerr << "Test for xRay tube failed!";
 
-	if (!test_detector()) cerr << "Test for detector failed!";
+	//if (!test_detector()) cerr << "Test for detector failed!";
 
-	if( !test_gantry() ) cerr << "Test for detector failed!";
+	//if( !test_gantry() ) cerr << "Test for detector failed!";
 
-	if( !test_testModel() ) cerr << "Test for test model failed!";
+	//if( !test_testModel() ) cerr << "Test for test model failed!";
 
 	if( !test_modelTransmission() ) cerr << "Test for model transmission failed!";
 
