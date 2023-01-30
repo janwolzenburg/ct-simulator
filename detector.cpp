85636e672fb71134b6968a26e1427dbbb044a12d
Jan Wolzenburg <kontakt@janwolzenburg.de>
2023-01-30 12:52:54 +0100

    Changed detector construction. Now with creatin of pixel

--- detector.cpp
+++ detector.cpp
@@ -127,102 +127,96 @@ detector::detector( cartCSys* const cSys_, const detectorRadonParameter radonPar
 	}
 
 
-	/*const size_t nTheta = 20;
-	const size_t nDistance = FOdd( 11 );
 
-	const double distanceRange = 10;
-	const double arcRadius = 20;
+	// Create pixel surfaces from pixel normals
 
-	const double deltaTheta = PI / (double) ( nTheta - 1 );
-	const double deltaDistance = distanceRange / (double) ( nDistance - 1 );
-
-	const uvec3 middleNormal = -cSys->EyVec();			// Middle normal is the negative y axis
-	const uvec3 rotationAxis = cSys->EzVec();				//Rotation axis is z axis
-
-	vector<line> pixelNormals;						// All normals
-	pixelNormals.push_back( line{ middleNormal, pnt3{ vec3{ -middleNormal } * arcRadius / 2 } } );			// The middle normal pointing from pixel to origin
-
-	//allPixel = vector<pixel>( nDistance );
-	//allPixel.push_back( pixel{  } )
-
-	// Iterate one "half" of normals
-	for( size_t currentIndex = 0; currentIndex < ( nDistance - 1 ) / 2; currentIndex++ ){
-
-		// Rotation angle to rotate the middle normal by
-		const double rotationAngle = (double) ( currentIndex + 1 ) * deltaTheta;
-
-		// Rotated normal
-		const uvec3 currentNormalVec = middleNormal.rotN( rotationAxis, rotationAngle );
-
-		// Lot on normal perpendicualr to rotation axis
-		vec3 normalLot = currentNormalVec ^ rotationAxis;
-		normalLot.normalize();	// Set length to one
-
-		// Distance from origin to normal
-		const double currentDistance = distanceRange / 2 - (double) ( ( nDistance - 1 ) / 2  - currentIndex - 1 ) * deltaDistance;
-
-		// Set lot length to distance
-		normalLot.scale( currentDistance );
-
-		// Point on Normal
-		const pnt3 normalPoint{ normalLot };
-
-		// Get point on pixel which lies on an arc with radius R
-		const primitiveVec3 o = normalPoint.XYZ();
-		const primitiveVec3 r = currentNormalVec.XYZ();
-		const double R = arcRadius;
-
-		const double p = ( 2*o.y*r.y + r.y*R + r.x ) / ( pow( r.y, 2 ) );
-		const double q = ( o.x + o.y*R + pow( o.y, 2 ) - 3/4*pow( R, 2 ) ) / pow( r.y, 2 );
-
-		const double lambda = -p/2 + sqrt( pow( p / 2, 2 ) - q );
+	// Iterate all normals from second to penultimate normal
+	for( auto currentNormalIt = allNormals.cbegin() + 1; currentNormalIt < allNormals.cend() - 1; currentNormalIt++ ){
 		
-		// The normal line of current pixel
-		const line currentNormal{ currentNormalVec, normalPoint };
-		pixelNormals.push_back( currentNormal );
+		auto previousNormalIt = currentNormalIt - 1;		// Previous normal
+		auto nextNormalIt = currentNormalIt + 1;			// Next normal
+	
+		
 
-		// Point on Pixel
-		//const pnt3 pointOnPixel = currentNormal.getPnt( lambda );
+		// To get the size of current pixel check intersection of neighbooring surface vectors
 
-		//const uvec3 surfVec1 = rotationAxis;
-		//const uvec3 surfVec2 = surfVec1 ^ currentNormalVec;
+		// Vector which lies on current surface and in xy-plane
+		const uvec3 currentSurfaceVector = currentNormalIt->R() ^ rotationVector;
 
-		//const surf pixelSurface{ surfVec1, surfVec2, pointOnPixel };
+		// Vector which lies on previous surface and in xy-plane
+		const uvec3 previousSurfaceVector = previousNormalIt->R() ^ rotationVector;
+
+		// Intersection of previous and current surface
+		const lineLine_Intersection currentPreviousIntersection{ 
+			line{ currentSurfaceVector, currentNormalIt->O() },
+			line{ previousSurfaceVector, previousNormalIt->O() }
+		};
+
+		// The point of intersection
+		const pnt3 currentPreviousIntersectionPoint = currentPreviousIntersection.Result().intersectionPoint;
+
+		// Small surface parameter as negative distance between intersection point and current surface's normal
+		const double currentPreviousParameter = -( currentPreviousIntersectionPoint - currentNormalIt->O() ).Length();
+
+
+
+		// Vector which lies on next surface and in xy-plane
+		const uvec3 nextSurfaceVector = nextNormalIt->R() ^ rotationVector;
+
+		// Intersection of next and current surface
+		const lineLine_Intersection currentNextIntersection{
+			line{ -currentSurfaceVector, currentNormalIt->O() },
+			line{ nextSurfaceVector, nextNormalIt->O() }
+		};
+
+		// The point of intersection
+		const pnt3 currentNextIntersectionPoint = currentPreviousIntersection.Result().intersectionPoint;
+
+		// Large surface parameter as distance between intersection point and current surface's normal
+		const double currentNextParameter = ( currentNextIntersectionPoint - currentNormalIt->O() ).Length();
+
+
+
+		// Create surfaces with calculated parameters
+		
+		const double colSize = indipendentParameter.columnSize;
+
+		// First surface. Add previous surface
+		if( currentNormalIt == allNormals.cbegin() + 1 ){
+
+			allPixel.emplace_back(	-currentSurfaceVector,
+									rotationVector,
+									previousNormalIt->O(),
+									currentPreviousParameter,
+									( currentPreviousIntersectionPoint - previousNormalIt->O() ).Length(),
+									-colSize / 2,
+								    colSize / 2 );
+		}
+
+
+		// Add the current pixel
+		allPixel.emplace_back(		-currentSurfaceVector,
+									rotationVector,
+									currentNormalIt->O(),
+									currentPreviousParameter,
+									currentNextParameter,
+									-colSize / 2,
+									colSize / 2 );
+
+
+		// Last surfac. Add next surface			
+		if( currentNormalIt == allNormals.cend() - 2 ){
+
+			allPixel.emplace_back(	-nextSurfaceVector,
+									rotationVector,
+									nextNormalIt->O(),
+									-( currentNextIntersectionPoint - nextNormalIt->O() ).Length(),
+									currentNextParameter,
+									-colSize / 2,
+									colSize / 2 );
+		}
 
 	}
-
-	// Initialise vectors
-	/*allPixel = vector<pixel>(physicalParameters.number.col);
-
-	// Calculate angle delta from pixel size along arc and arc radius 
-	double angleDelta = radonParameters.resolution.col;// 2 * atan( pxSize.r / ( 4 * radius ) );
-
-	// Iterate all columns
-	size_t columns = physicalParameters.number.col;
-	v2CR pxSize = physicalParameters.pixelSize;
-
-	for (size_t col = 0; col < columns; col++) {
-		double rotAngle = 0;
-
-		// Not the middle pixel
-		if (col != (columns - 1) / 2) {
-			// Rotation angle
-			rotAngle = -angleDelta * ((double)(columns - 1) / 2) + (double)col * angleDelta;
-		}
-		
-		const uvec3 n{ cSys->EyVec().rotZ(rotAngle) };		// Normal vector for row pixel in one column
-		const uvec3 r2 = cSys->EzVec();						// z-axis is r2 of surface
-		const pnt3 o = n * ( physicalParameters.detectorFocusDistance );					// Origin point of surface
-		const uvec3 r1 = n ^ r2;							// First direction vector
-
-		// Pixel with given normal vector centered at o + dZ
-		const pixel px{ r1, r2, o,    -pxSize.row / 2, pxSize.row / 2, -pxSize.col / 2, pxSize.col / 2 };
-
-		allPixel.at( col ) = px;
-
-	}*/
-
-
 }
 
 
