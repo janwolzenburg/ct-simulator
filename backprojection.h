768fdd62a585c177c28a94f9bcac2d2b64c2bb30
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-03-07 12:54:36 +0100

    Private base classes. Comments. Method to get copy of base grid

--- backprojection.h
+++ backprojection.h
@@ -29,16 +29,48 @@
 class filteredProjections : private grid {
 
 	public:
+
+	/*!
+	 * @brief Constructor
+	 * @param projections UNfiltered projections 
+	 * @param filterType Type of filter to apply
+	*/
 	filteredProjections( const radonTransformed projections, const discreteFilter::TYPE filterType );
 	
+	/*!
+	 * @brief Get value at angle index at given distance
+	 * @param angleIdx Angle index
+	 * @param distance Distance
+	 * @return Filtered value
+	*/
 	double getValue( const size_t angleIdx, const double distance ) const;
+
+	/*!
+	 * @brief Get base  grid
+	 * @return Grid
+	*/
+	inline grid getGrid( void ) const { return (grid) *this; };
 
 };
 
 
+/*!
+ * @brief Class to store reconstructed image
+*/
 class reconstrucedImage : private grid {
 
 	public:
+
+	/*!
+	 * @brief Constructor
+	 * @param projections Filtered projections 
+	*/
 	reconstrucedImage( const filteredProjections projections );
+
+	/*!
+	 * @brief Get base  grid
+	 * @return Grid
+	*/
+	inline grid getGrid( void ) const{ return (grid) *this; };
 
 };
