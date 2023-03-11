d1bb9a6d6aad060bc62ad053414a3a98fa98c5ab
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-03-11 21:20:47 +0100

    Added custom file chooser

---
+++ fileChooser.cpp
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * @file   fileChooser.cpp
+ * @brief  Implementations
+ *
+ * @author Jan Wolzenburg
+ * @date   March 2023
+ *********************************************************************/
+
+
+
+ /*********************************************************************
+   Includes
+*********************************************************************/
+
+#include "fileChooser.h"
+#include "FL/Fl_Native_File_Chooser.H"
+
+
+
+/*********************************************************************
+  Implementations
+*********************************************************************/
+
+
+
+fileChooser::fileChooser( const string windowTitle, const string fileFilter, const path defaultDirectory )
+{
+
+	this->type( Fl_Native_File_Chooser::BROWSE_FILE );
+	
+	this->title( windowTitle.c_str() );
+	this->filter( fileFilter.c_str() );
+
+	this->directory( defaultDirectory.string().c_str() );
+
+}
+
+
+path fileChooser::choose( void ){
+
+	if( this->show() != 0 ) return path{};
+
+	path filePath = path{ this->filename() };
+	this->directory( filePath.parent_path().string().c_str() );
+	
+	return filePath;
+
+}
---
+++ fileChooser.h
@@ -0,0 +1,50 @@
+#pragma once
+/*********************************************************************
+ * @file   fileChooser.h
+ * @brief  A custom file chooser based on FLTK native file chooser class
+ *
+ * @author Jan Wolzenburg
+ * @date   March 2023
+ *********************************************************************/
+
+
+ /*********************************************************************
+	Includes
+ *********************************************************************/
+
+ #include <string>
+ using std::string;
+
+ #include <filesystem>
+ using std::filesystem::path;
+
+#include "FL/Fl_Native_File_Chooser.H"
+
+
+
+ /*********************************************************************
+	Definitions
+ *********************************************************************/
+
+/*!
+ * @brief Custom class for choosing a file
+*/
+class fileChooser : private Fl_Native_File_Chooser{
+
+	public:
+
+	/*!
+	 * @brief Constructor
+	 * @param windowTitle Title of window 
+	 * @param fileFilter String with filter
+	 * @param defaultDirectory Directory to start
+	*/
+	fileChooser( const string windowTitle, const string fileFilter, const path defaultDirectory = path{  } );
+
+	/*!
+	 * @brief Choose a file
+	 * @return Path to chosen file
+	*/
+	path choose( void );
+		 
+ };
