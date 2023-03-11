00820b90f78bbcd4518e2be23ba7c33a5f1a9cb0
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-03-11 16:10:04 +0100

    Added gui hello_world program

--- main.cpp
+++ main.cpp
@@ -8,15 +8,27 @@
 #include <iostream>
 using std::cerr;  using std::endl;
 
- #include "test_all.h"
-
-
+ //#include "test_all.h"
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
 /*!
  * @brief Main function
  * @param  
  * @return 
 */
-int main( void ){
+int main( int argc, char** argv ){
+
+	Fl_Window* window = new Fl_Window( 340, 180 );
+	Fl_Box* box = new Fl_Box( 20, 40, 300, 100, "Hello, World!" );
+	box->box( FL_UP_BOX );
+	box->labelfont( FL_BOLD + FL_ITALIC );
+	box->labelsize( 36 );
+	box->labeltype( FL_SHADOW_LABEL );
+	window->end();
+	window->show( argc, argv );
+	return Fl::run();
+	
 
 	//if( !test_linear_algebra() ) cerr << "Test for linear algebra failed!";
 
@@ -53,7 +65,7 @@ int main( void ){
 	//test_scattered_angle_propabilities();
 
 
-	 test_ray_scattering(  );
+	 //test_ray_scattering(  );
 
 	return 0;
 }
