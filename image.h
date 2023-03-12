897b6e8f66732fd8ebff163fc70e444d302dfdde
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-03-12 19:51:15 +0100

    Image now has vectors instead of raw memory access

--- image.h
+++ image.h
@@ -27,14 +27,14 @@ class image{
 		width( source.Size().col ),
 		height( source.Size().row ),
 		numPixel( width * height ),
-		data( new double[numPixel] ),
-		imData( new char[numPixel] )
+		data( numPixel, 0. ),
+		imData( numPixel, 0. )
 	{
 		
 		for( size_t c = 0; c < width; c++ ){
 			for( size_t r = 0; r < height; r++ ){
 
-				data[ c + r * width ] = source.operator()(idx2CR{c, r});
+				data.at( c + r * width ) = source.operator()(idx2CR{c, r});
 			}
 		}
 
@@ -42,49 +42,15 @@ class image{
 
 	}
 
-	image( const image& sImg ) : 
-		width( sImg.width ),
-		height( sImg.height ),
-		numPixel( width* height ),
-		data( new double[numPixel] ),
-		imData( new char[numPixel] )
-	{
-		
-
-		memcpy( data, sImg.data, numPixel * sizeof( double ) );
-		memcpy( imData, sImg.imData, numPixel * sizeof( char ) );
-	};
-
-	image& operator()( const image& sImg ){
-		width = sImg.width;
-		height = sImg.height;
-
-		numPixel = width * height;
-		double* tempData = new double[numPixel];
-		char* tempImData = new char[numPixel];
-
-		delete[] data;
-		delete[] imData;
-
-		data = tempData;
-		memcpy( data, sImg.data, numPixel * sizeof( double ) );
-
-		imData = tempImData;
-		memcpy( imData, sImg.imData, numPixel * sizeof( char ) );
-
-		return *this;
-	}
-
-	~image( void ){ delete[] data; };
-
 
 	private:
 	size_t width;
 	size_t height;
 	size_t numPixel;
 
-	double* data;
-	char* imData;
+
+	vector<double> data;
+	vector<char> imData;
 
 
 };
