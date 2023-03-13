cf323ecc7d9393ef7b83a63cd012a8d2973e0670
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-03-13 11:51:10 +0100

    Executed test

--- main.cpp
+++ main.cpp
@@ -19,6 +19,16 @@ using std::cerr;  using std::endl; using std::cout;
 #include "surf.h"
 #include "image.h"
 
+class Fl_Image_Widget : public Fl_Widget{
+
+	public:
+
+	Fl_Image_Widget( void );
+
+	
+
+};
+
 /*!
  * @brief Main function
  * @param  
@@ -28,31 +38,39 @@ int main( int argc, char** argv ){
 
 	programState& currentState = PROGRAM_STATE();
 
-	if( !currentState.ModelLoaded() )currentState.loadModel();
+	if( !currentState.ModelLoaded() ) currentState.loadModel();
 
 
-	model& currentModel = currentState.Model();
+	//model& currentModel = currentState.Model();
 
-	cartCSys* viewSystem = CSYS_TREE().addCSys("View system");
+	//cartCSys* viewSystem = CSYS_TREE().addCSys("View system");
 
-	const surfLim viewPlane{	uvec3{ v3{ 1, 0, 0 }, viewSystem },
-								uvec3{ v3{ 0, 1, 0 }, viewSystem },
-								pnt3{  currentModel.ModSize(), viewSystem } / 2.,
-								-currentModel.ModSize().x / 2., currentModel.ModSize().x / 2.,
-								-currentModel.ModSize().y / 2., currentModel.ModSize().y / 2. };
+	//const surfLim viewPlane{	uvec3{ v3{ 1, 0, 0 }, viewSystem },
+	//							uvec3{ v3{ 0, 1, 0 }, viewSystem },
+	//							pnt3{  currentModel.ModSize(), viewSystem } / 2.,
+	//							-currentModel.ModSize().x / 2., currentModel.ModSize().x / 2.,
+	//							-currentModel.ModSize().y / 2., currentModel.ModSize().y / 2. };
 
-	const grid modelSlice = currentModel.getSlice( viewPlane, 1. );
+	//const grid modelSlice = currentModel.getSlice( viewPlane, 1. );
 
-	image sliceImage{ modelSlice };
+	//image sliceImage{ modelSlice };
+
+	//vector<char> imageBinary;
+	//sliceImage.serialize( imageBinary );
+
+	//exportSerialized( "modelSliceImage.image", imageBinary ); 
 
 	Fl_Window* window = new Fl_Window( (int) ( 500. * 16. / 9. ), 500 );
 
+	image& sliceImage = currentState.ModelSlice();
 	vector<unsigned char> imageBufferMono = sliceImage.getImage();
+	
+	Fl_Box* sliceImageBox = new Fl_Box( 0, 0, (int) sliceImage.Width(), (int) sliceImage.Heigth() );
+	window->add( sliceImageBox );
+	//
+	Fl_RGB_Image* slice = new Fl_RGB_Image( imageBufferMono.data(), (int) sliceImage.Width(), (int) sliceImage.Heigth(), 1 );
 
-	Fl_Box* sliceImageBox = new Fl_Box( 0, 0, (int) modelSlice.Size().col, (int) modelSlice.Size().row );
-	Fl_RGB_Image* slice = new Fl_RGB_Image( imageBufferMono.data(), (int) modelSlice.Size().col, (int) modelSlice.Size().row, 1 );
-
-	//fl_draw_image_mono( imageBuffer.data(), 0, 0, (int) modelSlice.Size().col, (int) modelSlice.Size().row);
+	////fl_draw_image_mono( imageBufferMono.data(), 0, 0, (int) modelSlice.Size().col, (int) modelSlice.Size().row,);
 	//slice->draw(0, 0, (int) modelSlice.Size().col, (int) modelSlice.Size().row );
 
 	sliceImageBox->image( slice );
