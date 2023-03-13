53f0d8836ecd35e764ebbde5d60725b3eb494e75
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-03-13 13:21:34 +0100

    Added constructor which scales given grey image

--- image.h
+++ image.h
@@ -23,9 +23,9 @@ class greyImage{
 
 	public:
 
-	greyImage( void ) :
-		width( 0 ),
-		height( 0 ),
+	greyImage( const size_t width_, const size_t height_ ) :
+		width( width_ ),
+		height( height_ ),
 		numPixel( width* height ),
 		dataLocked( false ),
 		data( numPixel, 0. ),
@@ -34,13 +34,16 @@ class greyImage{
 
 	}
 
+	greyImage( void ) :
+		greyImage{ 0, 0 }
+	{
+
+	}
+
+
+
 	greyImage( const grid source ) : 
-		width( source.Size().col ),
-		height( source.Size().row ),
-		numPixel( width * height ),
-		dataLocked( false ),
-		data( numPixel, 0. ),
-		imData( numPixel, 0 )
+		greyImage{ source.Size().col, source.Size().row }
 	{
 		
 		for( size_t c = 0; c < width; c++ ){
@@ -49,19 +52,35 @@ class greyImage{
 			}
 		}
 
-		const double maxVal = Max( data );
-		const double minVal = Min( data );
-		
-		for( size_t i = 0; i < numPixel; i++ ){
-			imData.at( i ) = (unsigned char) ( ( ( data.at( i ) - minVal ) / ( maxVal - minVal ) ) * 255. );
+		normalize();
+
+	};
+
+	greyImage( const greyImage& srcImg, const size_t newWidth, const size_t newHeight ) :
+		greyImage{ newWidth, newHeight }
+	{
+
+		for( size_t c = 0; c < this->Width(); c++ ){
+
+			size_t srcC = (size_t) ( (double) c * ( (double) srcImg.Width() - 1. ) / ( (double) this->Width() - 1. ) );
+
+			for( size_t r = 0; r < this->Height(); r++ ){
+
+				size_t srcR = (size_t) ( (double) r * ( (double) srcImg.Height() - 1. ) / ( (double) this->Height() - 1. ) );
+
+				this->operator()( c, r ) = srcImg( srcC, srcR );
+
+			}
 		}
 
-	}
+		normalize();
+
+	};
 
 	greyImage( const vector<char>& binData, vector<char>::const_iterator& it ) : 
 		width( deSerializeBuildIn( (size_t) 1, binData, it ) ),
 		height( deSerializeBuildIn( (size_t) 1, binData, it ) ),
-		numPixel( width* height ),
+		numPixel( width * height ),
 		dataLocked( false ),
 		data( numPixel, 0. ),
 		imData( numPixel, 0 )
@@ -71,14 +90,8 @@ class greyImage{
 			data.at( i ) = deSerializeBuildIn( 0., binData, it );
 		}
 
-		if( data.size() == 0 ) return;
-
-		const double maxVal = Max( data );
-		const double minVal = Min( data );
-
-		for( size_t i = 0; i < numPixel; i++ ){
-			imData.at( i ) = (unsigned char) ( ( ( data.at( i ) - minVal ) / ( maxVal - minVal ) ) * 255. );
-		}
+		
+		normalize();
 
 	};
 
@@ -93,12 +106,30 @@ class greyImage{
 		data = srcImg.data;
 		imData = srcImg.imData;
 
+		return *this;
 	};
 
+	double operator()( const size_t c, const size_t r ) const{
+	
+		size_t idx = c + r * width;
+		if( idx >=  numPixel ) idx = numPixel - 1;
+	
+		return data.at( idx );
+
+	}
+
+	double& operator()( const size_t c, const size_t r ){
+
+		size_t idx = c + r * width;
+		if( idx >= numPixel ) idx = numPixel - 1;
+
+		return data.at( idx );
+
+	}
 
 	inline size_t Width( void ) const{ return width; };
 	
-	inline size_t Heigth( void ) const{ return height; };
+	inline size_t Height( void ) const{ return height; };
 	
 	vector<unsigned char> getImage( void ) const{ return imData; };
 	
@@ -106,6 +137,8 @@ class greyImage{
 		dataLocked = true;
 		return imData.data(); 
 	};
+
+	inline void unlockData( void ) { dataLocked = false; };
 
 	/*!
 	 * @brief Serialize this object
@@ -125,7 +158,7 @@ class greyImage{
 	};
 
 
-	protected:
+	private:
 	size_t width;
 	size_t height;
 	size_t numPixel;
@@ -134,5 +167,20 @@ class greyImage{
 	vector<double> data;
 	vector<unsigned char> imData;
 
+
+	private:
+
+	void normalize( void ){
+
+		if( data.size() == 0 ) return;
+
+		const double maxVal = Max( data );
+		const double minVal = Min( data );
+
+		for( size_t i = 0; i < numPixel; i++ ){
+			imData.at( i ) = (unsigned char) ( ( ( data.at( i ) - minVal ) / ( maxVal - minVal ) ) * 255. );
+		}
+
+	};
 
 };
