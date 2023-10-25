77b2b9df64eb1110f7f5937b4e02061581766f1d
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-10-25 11:34:46 +0200

    Moved code away and substituded assert with enable_if

--- widgets.h
+++ widgets.h
@@ -12,21 +12,8 @@
 	Includes
  *********************************************************************/
 
-#include <FL/Fl_Box.H>
-
-#include <FL/Fl_Group.H>
-#include <FL/Fl_Counter.H>
-#include <FL/Fl_Button.H>
-#include <FL/Fl_Output.H>
-#include <FL/Fl_Float_Input.H>
-#include <FL/Fl_Tooltip.H>
-
-#include "Fl_GridImage.h"
-#include "Fl_OwnValuator.h"
-#include "Fl_Plot.h"
-
-
-
+ #include <FL/Fl_Widget.H>
+ #include<type_traits>
 
  /*********************************************************************
 	Definitions
@@ -34,22 +21,22 @@
 
 void button_cb( Fl_Widget* widget, void* flag );
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool> = false>
 int X( const C& parent, const double start );
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool> = false>
 int Y( const C& parent, const double start );
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool> = false>
 int W( const C& parent, const double portion );
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool> = false>
 int H( const C& parent, const double portion );
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool> = false>
 int vOff( const C& parent );
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool> = false>
 int hOff( const C& parent );
 
 #include "widgets.hpp"
--- widgets.hpp
+++ widgets.hpp
@@ -9,38 +9,32 @@
 #include "widgets.h"
 
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool>>
 int X( const C& parent, const double start ){
-	static_assert( std::is_base_of_v< Fl_Widget, C > );
 	return parent.x() + (int) ( start * (double) parent.w() );
 }
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool>>
 int Y( const C& parent, const double start ){
-	static_assert( std::is_base_of_v< Fl_Widget, C > );
 	return parent.y() + (int) ( start * (double) parent.h() );
 }
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool>>
 int W( const C& parent, const double portion ){
-	static_assert( std::is_base_of_v< Fl_Widget, C > );
 	return (int) ( portion * (double) parent.w() );
 }
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool>>
 int H( const C& parent, const double portion ){
-	static_assert( std::is_base_of_v< Fl_Widget, C > );
 	return (int) ( portion * (double) parent.h() );
 }
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool>>
 int vOff( const C& parent ){
-	static_assert( std::is_base_of_v< Fl_Widget, C > );
 	return parent.y() + parent.h();
 }
 
-template<class C>
+template<class C, std::enable_if_t<std::is_base_of_v< Fl_Widget, C >, bool>>
 int hOff( const C& parent ){
-	static_assert( std::is_base_of_v< Fl_Widget, C > );
 	return parent.x() + parent.w();
 }
