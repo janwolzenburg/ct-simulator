f12e01405cd06c3bc35374d2e5caed99cb92f1b9
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-10-31 10:05:45 +0100

    Added comment

--- tomography.cpp
+++ tomography.cpp
@@ -111,8 +111,8 @@ Projections Tomography::RecordSlice( const ProjectionsProperties radon_propertie
 			// Get Coordinates for pixel
 			const RadonCoordinates newRadonCoordinates{ this->radon_coordinate_system_, currentPixel.NormalLine() };
 
+			// Correction factor necessary because a finite amount of rays are simulated and scattering may lead to large drop in detected intensity
 			const size_t pixel_hits = currentPixel.detected_ray_properties().size();
-
 			const double correction_factor = pixel_hits > 1 ? 1. / static_cast<double>( pixel_hits ) : 1.;
 
 			// Get the radon point
