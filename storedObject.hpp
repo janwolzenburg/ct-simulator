1d3c31e438c526e1d639b5acc5caa4593f5abb5c
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-11-01 10:01:04 +0100

    Using move for every class

--- storedObject.hpp
+++ storedObject.hpp
@@ -29,11 +29,8 @@ bool storedObject<C>::load( const path file_path ){
 	vector<char>::iterator binaryDataIt = binaryData.begin();
 
 	if( !ValidBinaryData( object.FILE_PREAMBLE, binaryData, binaryDataIt ) ) return false;
-
-	if constexpr( std::is_same_v<C, Model> )
-		object = std::move( C{ binaryData, binaryDataIt } );
-	else
-		object = C{ binaryData, binaryDataIt };
+	
+	object = std::move( C{ binaryData, binaryDataIt } );
 	
 	loaded = true;
 	return loaded;
