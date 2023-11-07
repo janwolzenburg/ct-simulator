1e8d4894161cac38e573c65e847d89c71ae0ad79
Jan Wolzenburg <kontakt@janwolzenburg.de>
2023-11-07 16:17:11 +0100

    Handling for pixel without detected ray

--- detectorPixel.cpp
+++ detectorPixel.cpp
@@ -32,12 +32,16 @@ double DetectorPixel::GetRadonValue( void ) const{
 	for( const RayProperties& currentRay : detected_ray_properties_ ){
 		const double current_intensity = currentRay.energy_spectrum_.GetTotal();
 		
-
+		// TODO: Check maths whether addition like ist is valid
 		sum_of_line_integrals += log( currentRay.start_intensity() / current_intensity );
 	}
 
 	// Correction factor necessary because a finite amount of rays are simulated and scattering may lead to large drop in detected intensity
-	const size_t pixel_hits = detected_ray_properties().size();
+	const size_t pixel_hits = detected_ray_properties_.size();
+	if( pixel_hits == 0 ){
+		sum_of_line_integrals = 1; // make this raysperPixel
+	}
+	
 	const double correction_factor = pixel_hits > 1 ? 1. / static_cast<double>( pixel_hits ) : 1.;
 
 	// Correction factor necessary because a finite amount of rays are simulated and scattering may lead to large drop in detected intensity
