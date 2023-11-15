f3b0e8ff1dbbe55ee37f76f9d8219c564d2b4715
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-11-15 15:40:46 +0100

    Changed ditance range calculation

--- fl_GantryCreation.cpp
+++ fl_GantryCreation.cpp
@@ -168,8 +168,8 @@ Fl_GantryCreation::Fl_GantryCreation( int x, int y, int w, int h, Fl_MainWindow&
 }
 
 void Fl_GantryCreation::SetDistances( const double max_corner_distance ){
-	detector_focus_distance_input_.value( 2.15 * ( max_corner_distance ) );
-	distance_range_input_.value( 2.05 * ( max_corner_distance ) );
+	detector_focus_distance_input_.value( ceil( 2.75 * ( max_corner_distance ) ) );
+	distance_range_input_.value( ceil( 2.5 * ( max_corner_distance ) ) );
 
 	UpdateGantry();
 }
