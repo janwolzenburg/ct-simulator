ba233b9fb0bd0243bb14d582fd004a4f86b6e5a2
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-11-15 14:38:53 +0100

    debug model now uses mu of water and bone

--- main.cpp
+++ main.cpp
@@ -31,15 +31,15 @@ int main( [[maybe_unused]] int argc, [[maybe_unused]] char** argv ){
 	/*
 	Model debugModel{ GlobalSystem()->CreateCopy( "Model system"), Index3D{3, 3, 1}, Tuple3D{1., 1., 1.}, string{"Debug model"}};
 	
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 0, 0, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 0, 1, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 0, 2, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 1, 0, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .1, 100000., }, Index3D{ 1, 1, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 1, 2, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 2, 0, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 2, 1, 0 } );
-	debugModel.SetVoxelData( VoxelData{ .01, 100000., }, Index3D{ 2, 2, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 0, 0, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 0, 1, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 0, 2, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 1, 0, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_bone, 100000., }, Index3D{ 1, 1, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 1, 2, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 2, 0, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 2, 1, 0 } );
+	debugModel.SetVoxelData( VoxelData{ mu_water, 100000., }, Index3D{ 2, 2, 0 } );
 	
 
 	PersistingObject<Model> saveableModel{ Model{}, path{"./debug.model"} };
