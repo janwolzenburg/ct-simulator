1633b385ccdc606a6cb6d1a6d28dc5da0b1d9cec
Jan Wolzenburg <kontakt@janwolzenburg.de>
2024-02-29 14:14:56 +0100

    Changed scattering parameters

--- simulation.h
+++ simulation.h
@@ -26,11 +26,11 @@ constexpr size_t default_max_radiation_loops = 1;							/*!< How often can a Ray
 constexpr double default_max_ray_angle_allowed_by_structure = 5. / 360. * 2. * PI;	/*!< Default maximum arc_angle between ray and pixel normal allowed by anti scattering structure*/
 
 
-constexpr size_t number_of_points_in_tube_spectrum_ = 128;		/*!< Amount of discrete datapoints in spectrum*/
+constexpr size_t number_of_points_in_tube_spectrum_ = 32;		/*!< Amount of discrete datapoints in spectrum*/
 
-constexpr size_t number_of_scatter_angles = 255;								/*!< Amount of discrete angles available for scattering - should be odd to include 0°*/
+constexpr size_t number_of_scatter_angles = 63;								/*!< Amount of discrete angles available for scattering - should be odd to include 0°*/
 constexpr size_t number_of_energies_for_scattering = 128;					/*!< Amount of energies used */
 
-constexpr size_t bins_per_energy = 32;		/*!< When scattering: Into how many bins should the photons with the same energy be divided into*/
+constexpr size_t bins_per_energy = 16;		/*!< When scattering: Into how many bins should the photons with the same energy be divided into*/
 constexpr double minimum_energy_in_tube_spectrum = 10000.;
 constexpr double maximum_energy_in_tube_spectrum = 210000.;
