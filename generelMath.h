f0cb8d918a58c9351bb4011d4efeaec401058524
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2024-04-04 11:43:24 +0200

    Changed comments

--- generelMath.h
+++ generelMath.h
@@ -25,46 +25,45 @@ using std::string;
 	Definitions
  *********************************************************************/
 
-constexpr double PI =   3.1415926535897932384626433832795;		/*!< PI */
-constexpr double PI_2 = 9.8696044010893586188344909998761;		/*!< PI ^ 2*/
-constexpr double c_mPers = 299792456;							/*!< Speed of light in m/s */
-constexpr double e_As = 1.6021917E-19;							/*!< Electron charge in As */
-constexpr double J_Per_eV = e_As;								/*!< Joules in one eV */
-constexpr double h_Js = 6.626196E-34;							/*!< Planck's constant */
-constexpr double h_eVs = 4.1357E-15;							/*!< Planck's constant */
-constexpr double h_c_eVmm = c_mPers * h_eVs * 1000.;			/*!< Product of h and c in eV*mm*/
+constexpr double PI =   3.1415926535897932384626433832795;		/*!< pi */
+constexpr double PI_2 = 9.8696044010893586188344909998761;		/*!< pi ^ 2*/
+constexpr double c_mPers = 299792456;													/*!< speed of light in m/s */
+constexpr double e_As = 1.6021917E-19;												/*!< electron charge in As */
+constexpr double J_Per_eV = e_As;															/*!< joules in one eV */
+constexpr double h_Js = 6.626196E-34;													/*!< planck's constant */
+constexpr double h_eVs = 4.1357E-15;													/*!< planck's constant */
+constexpr double h_c_eVmm = c_mPers * h_eVs * 1000.;					/*!< product of h and c in eV*mm*/
 
-constexpr double me_c2_eV = 0.511e6;							/*!< Compton Wavelength in eV */
-constexpr double me_c2_J = me_c2_eV * J_Per_eV;					/*!< Compton wavelenth in Joule */
-constexpr double Per_me_c2_eV = 1 / me_c2_eV;					
+constexpr double me_c2_eV = 0.511e6;													/*!< compton Wavelength in eV */
+constexpr double me_c2_J = me_c2_eV * J_Per_eV;								/*!< compton wavelenth in Joule */
+constexpr double Per_me_c2_eV = 1 / me_c2_eV;									/*!< reciprocal of compton wavelength*/
 
 constexpr double permitivity_As_Per_Vm = 8.8541878128e-12;		/*!< epsilon0: Permitivity of vaccum in As/Vm */
-constexpr double r_e_mm = 2.8179403262e-12;						/*!< Electron radius in mm*/
-constexpr double reduced_energy_divisor_eV = 510998.95;			/*!< Divisor in reduced energy calculation m_e * c^2*/
+constexpr double r_e_mm = 2.8179403262e-12;										/*!< electron radius in mm*/
+constexpr double reduced_energy_divisor_eV = 510998.95;				/*!< divisor in reduced energy calculation m_e * c^2*/
 
-constexpr double reference_energy_for_mu_eV = 100000.;
-constexpr double absorption_water_Per_mm = 3e-2 * 0.997 / 10.;					/*!< Absorption Water in 1 / mm for 100keV*/
-constexpr double mu_air = 0.;
-constexpr double absorption_bone_Per_mm = absorption_water_Per_mm * 1.33;
-constexpr double absorption_lung_Per_mm = absorption_water_Per_mm * 0.8;
-constexpr double absorption_titan_Per_mm = 0.0135;
+constexpr double reference_energy_for_mu_eV = 100000.;										/*!< reference energy for stored coefficients*/
+constexpr double absorption_water_Per_mm = 3e-2 * 0.997 / 10.;						/*!< absorption Water in 1 / mm for 100keV*/
+constexpr double mu_air = 0.;																							/*!< absorption coefficient of air*/
+constexpr double absorption_bone_Per_mm = absorption_water_Per_mm * 1.33;	/*!< absorption coefficient of bone tissue*/
+constexpr double absorption_lung_Per_mm = absorption_water_Per_mm * 0.8;	/*!< absorption coefficient of lung tissue*/
+constexpr double absorption_titan_Per_mm = 0.0135;												/*!< absorption coefficient of titan*/
 
-constexpr double attenuation_water_Per_mm = 0.153 / 10;
+constexpr double attenuation_water_Per_mm = 0.153 / 10;										/*!< attenuation coefficient of water*/
 
+constexpr double change_energy_for_constant_mu = 52e3;	/*!< energy when absorption becomes constant*/
 
-constexpr double change_energy_for_constant_mu = 52e3;
+constexpr double avogadro_1Permol = 6.0221367e23;				/*!< avogardo's constant in 1/mol*/
 
-constexpr double avogadro_1Permol = 6.0221367e23;				/*!< Avogardo's constant in 1/mol*/
-
-constexpr double electron_density_water_1Permm3 =				/*!< Electron density or Conversion factor from cross section to absorption coefficiant for water in 1 / mm^3*/
+constexpr double electron_density_water_1Permm3 =				/*!< electron density or Conversion factor from cross section to absorption coefficiant for water in 1 / mm^3*/
 				 10. *		// electrons per molecule
-				 .998	*	// g/cm^3
+				 .998	*		// g/cm^3
 				 avogadro_1Permol /
 				 18. * 1e-3; // mass of one mol cm^3/mm^3
 
 
-inline std::ostream& math_error_out = std::cerr;					/*!< Outstream for errors */
-inline std::ostream& standard_out = std::cout;						/*!< Standard out stream */
+inline std::ostream& math_error_out = std::cerr;					/*!< outstream for errors */
+inline std::ostream& standard_out = std::cout;						/*!< standard out stream */
 
 
 
@@ -72,97 +71,100 @@ inline std::ostream& standard_out = std::cout;						/*!< Standard out stream */
 	Functions
  *********************************************************************/
 
+/*!
+ * @brief modes for numerical comparison
+ */
 enum ComparisonMode{
 	Absolute,
 	Relative
 };
 
 /*!
- * @brief Compares to values for their equality with tolerance
+ * @brief compares to values for their equality with tolerance
  * @param a Value 1
  * @param b Value 2
- * @param tolerance Maximum difference between a and b for them to be considered equal. Absolute or relative depending on next argument
- * @param mode Comparison mode: tolerance is absolute difference or relative difference
- * @return True when the difference is less than tolerance
+ * @param tolerance maximum difference between a and b for them to be considered equal. Absolute or relative depending on next argument
+ * @param mode comparison mode: tolerance is absolute difference or relative difference
+ * @return true when the difference is less than tolerance
 */
 bool IsNearlyEqual( const double a, const double b, const double tolerance, const ComparisonMode mode );
 
 /*!
- * @brief Compares to values for their equality with tolerance
+ * @brief compares to values for their equality with tolerance
  * @param a Value 1
  * @param b Value 2
- * @return True when the difference is less than EPSILON
+ * @return true when the difference is less than EPSILON
 */
 bool IsNearlyEqualDistance( const double a, const double b );
 
 /*!
- * @brief Get the relative deviation between two values
+ * @brief get the relative deviation between two values
  * @tparam T 
  * @param a 
  * @param b 
- * @return 
+ * @return relative deviation
 */
 template <typename T>
 double RelativeDeviation( const T a, const T b );
 
 /*!
- * @brief Checks if number_of_pixel is even
- * @tparam T Integer type
+ * @brief checks if number is even
+ * @tparam T integer type
  * @param integer Value
- * @return True when integer is even
+ * @return true when integer is even
 */
 template <typename T>
 typename std::enable_if_t<std::is_arithmetic_v<T>, bool>
 IsEven( const T integer );
 
 /*!
- * @brief Return the minimum of two values
+ * @brief return the minimum of two values
  * @tparam T Arithmetic type
  * @param a Value a
  * @param b Value b
- * @return Minimum value
+ * @return minimum value
 */
 template <typename T>
 typename std::enable_if_t<std::is_arithmetic_v<T>, T>
 Min( const T a, const T b );
 
 /*!
- * @brief Return the maximum of two values
- * @tparam T Arithmetic type
- * @param a Value a
- * @param b Value b
- * @return Maximum value
+ * @brief return the maximum of two values
+ * @tparam T arithmetic type
+ * @param a value a
+ * @param b value b
+ * @return maximum value
 */
 template <typename T>
 typename std::enable_if_t<std::is_arithmetic_v<T>, T> 
 Max( const T a, const T b );
 
 /*!
- * @brief Force a maximum value
- * @tparam T Arithmetic type
- * @param value Value to force 
+ * @brief force a maximum value
+ * @tparam T arithmetic type
+ * @param value value to force 
  * @param maximum upper bound of value
- * @return value when value is smaller than maximum. Maximum otherwise
+ * @return value when value is smaller than maximum. maximum otherwise
 */
 template <typename T>
 typename std::enable_if_t<std::is_arithmetic_v<T>, T>
 ForceToMax( const T value, const T maximum );
 
 /*!
- * @brief Force a minimum value
- * @tparam T Arithmetic type
+ * @brief force a minimum value
+ * @tparam T arithmetic type
  * @param value Value
- * @param  m minimum value
- * @return a or minValue when a is less than minValue
+ * @param  minimum minimum value
+ * @return a or minimum when a is less than minimum
 */
 template <typename T>
 typename std::enable_if_t<std::is_arithmetic_v<T>, T>
 ForceToMin( T value, T minimum );
 
 /*!
- * @brief Force a minimum value of one
- * @tparam T Arithmetic type
- * @param a Value a
+ * @brief force a minimum value of one
+ * @tparam T arithmetic type
+ * @param a value a
  * @return a when a is greater than one. 1 otherwise
 */
 template <typename T>
@@ -170,9 +172,9 @@ typename std::enable_if_t<std::is_arithmetic_v<T>, T>
 ForceToMin1( T a );
 
 /*!
- * @brief Force value to be positive
- * @tparam T Arithmetic type
- * @param a Value a
+ * @brief force value to be positive
+ * @tparam T arithmetic type
+ * @param a value a
  * @return a when a is greater than 0. 1 otherwise
 */
 template <typename T>
@@ -180,8 +182,8 @@ typename std::enable_if_t<std::is_arithmetic_v<T>, T>
 ForcePositive( T a );
 
 /*!
- * @brief Force value to an odd value
- * @tparam T Integral type
+ * @brief force value to an odd value
+ * @tparam T integral type
  * @param a Value
  * @return a when add. a + 1 when even
 */
@@ -190,22 +192,22 @@ typename std::enable_if_t<std::is_integral_v<T>, T>
 ForceOdd( T a );
 
 /*!
- * @brief Force value to an even value
- * @tparam T Integral type
+ * @brief force value to an even value
+ * @tparam T integral type
  * @param a Value
  * @return a when add. a + 1 when even
 */
 template <typename T>
 typename std::enable_if_t<std::is_integral_v<T>, T>
-Forceeven( T a );
+ForceEven( T a );
 
 /*!
- * @brief Force value to a range
- * @tparam T Arithmetic type
+ * @brief force value to a range
+ * @tparam T arithmetic type
  * @param value Value
- * @param minimum Minimum value
- * @param maximum Maximum value
- * @return Value constraint to range
+ * @param minimum minimum value
+ * @param maximum maximum value
+ * @return value constraint to range
 */
 template <typename T>
 typename std::enable_if_t<std::is_arithmetic_v<T>, T>
@@ -213,61 +215,62 @@ ForceRange( const T value, const T minimum, const T maximum );
 
 
 /*********************************************************************
-	Classes
+	classes
  *********************************************************************/
 
 /*!
- * @brief Abstract class for mathematical objects
- * @details Class with function prototypes to convert object's data to strings, dumping the data and outputting error messages
+ * @brief abstract class for mathematical objects
+ * @details class with function prototypes to convert object's data to strings, dumping the data and outputting error messages
 */
 class MathematicalObject{
+
 	public:
 
 	/*!
-	 * @brief Enumeration of error codes
+	 * @brief enumeration of error codes
 	*/
 	enum class MathError{
-		Ok,				/*!<  No error */
-		General,		/*!<  Abstract math error */
-		Operation,		/*!<  Error during mathematical operation */
-		Bounds,			/*!<  Out of bounds error */
-		Input			/*!<  Invalid input in function */
+		Ok,					/*!< no error */
+		General,		/*!< abstract math error */
+		Operation,	/*!< error during mathematical operation */
+		Bounds,			/*!< out of bounds error */
+		Input				/*!< invalid input in function */
 	};
 
 	/*!
-	 * @brief Convert object's data to string
+	 * @brief convert object's data to string
 	 * @param newline_tabulators Amount of tabulators to insert after each Line break
-	 * @return String with object's data_
-	 * @details Virtual for object specific implementation
+	 * @return string with object's data
+	 * @details virtual for object specific implementation
 	*/
 	virtual string ToString( [[maybe_unused]] const unsigned int newline_tabulators = 0 ) const = 0;
 
 	/*!
-	 * @brief Dump object's type and data to output stream
+	 * @brief dump object's type and data to output stream
 	 * @param output Output stream to dump to
-	 * @param objName Name of the object to include in the dump
+	 * @param object_name Name of the object to include in the dump
 	*/
-	void Dump( std::ostream& output, const string objName ) const;
+	void Dump( std::ostream& output, const string object_name ) const;
 
 	/*!
-	 * @brief Convert last error to string<
+	 * @brief convert last error to string
 	 * @param code Error code
-	 * @return String describing error
+	 * @return string describing error
 	*/
 	string GetErrorString( const MathError code ) const;
 
 	/*!
-	 * @brief Outputs error string to stream when it's not MATH_ERR::OK
-	 * @param code Error code
-	 * @return Same as code
+	 * @brief outputs error string to stream when it's not MATH_ERR::OK
+	 * @param code error code
+	 * @return same as code
 	*/
 	MathError CheckForAndOutputError( const MathError code ) const;
 
 	/*!
-	 * @brief Outputs error string to stream when it's not MATH_ERR::OK
+	 * @brief outputs error string to stream when it's not MATH_ERR::OK
 	 * @param code Error code
 	 * @param message Message to output on mathErrOut
-	 * @return Same as code
+	 * @return same as code
 	*/
 	MathError CheckForAndOutputError( const MathError code, const string message ) const;
 };
