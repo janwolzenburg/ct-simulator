ce75aa4dbef718f14090f3e73f815ec0c39adb3d
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2024-04-04 10:33:18 +0200

    Higher quality now possible

--- simulation.cpp
+++ simulation.cpp
@@ -24,8 +24,8 @@ SimulationProperties simulation_properties{ 20 };
 const string SimulationProperties::FILE_PREAMBLE{ "SimulationProperties_FILE_PREAMBLE" };
 
 SimulationProperties::SimulationProperties( const size_t simulation_quality ) :
-quality( ForceRange( simulation_quality, 0ull, 49ull) ),
-	ray_step_size_mm( 1e-2 / ( 4. * static_cast<double>( quality + 1 ) ) ),
+quality( ForceRange( simulation_quality, 0ull, 99ull) ),
+	ray_step_size_mm( ForceToMin( 1e-2 / ( 4. * static_cast<double>( quality + 1 ) ), 1e-3 ) ),
 	number_of_points_in_spectrum_( 8 + 3 * quality / 2),
 	number_of_energies_for_scattering( 16 * ( quality + 1 ) ),
 	number_of_scatter_angles( ForceOdd( 16 + 2 * quality ) ),
